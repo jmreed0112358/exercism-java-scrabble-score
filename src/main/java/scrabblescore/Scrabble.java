c9a4fec14bae75ed5b4831f5db37691bade76640
Jeremy M. Reed <reeje76@gmail.com>
2016-06-28 18:43:26 -0600

    Project setup
    Import into Eclipse.
    Convert to Maven project.
    Add JUnit dependency.
    Move sources to proper packages.
    Add NotImplementedException RuntimeException.
    Stub out Scrabble methods.

---
+++ src/main/java/scrabblescore/Scrabble.java
@@ -0,0 +1,12 @@
+package scrabblescore;
+
+import exceptions.NotImplementedException;
+
+public class Scrabble
+{
+
+	public Scrabble( String input ) {
+		throw new NotImplementedException();
+	}
+
+}
--- src/test/java/scrabblescore/ScrabbleScoreTest.java
+++ src/test/java/scrabblescore/ScrabbleScoreTest.java
@@ -1,3 +1,4 @@
+package scrabblescore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
